eaecff8b50c578d30180f95e6faf96d48bfc9e81
Georgio Cha <92047233+SassyBaker@users.noreply.github.com>
2024-09-28 11:42:24 -0600

    Update gpt_api_call.py
    Added comments and removed extra code

--- gpt_api_call.py
+++ gpt_api_call.py
@@ -2,24 +2,13 @@
 # from .gpt_api_call import call_gpt
 from ollama import Client
 
-server_ip = 'http://198.166.143.190:11434'
-
-# gpt_model = 'llama3.1'  # ChatGPT equivalent
-gpt_model = 'qwen2.5-coder'  # More optimised for code
+server_ip = 'http://198.166.143.190:11434'  # GPT API Endpoint
+gpt_model = 'qwen2.5-coder'  # General GPT "llama3.1"
 
 
 def call_gpt(prompt):
     client = Client(host=server_ip)
     response = client.chat(model='llama3.1', messages=[
-        {
-            'role': 'user',
-            'content': prompt,
-        },
-    ])
+        {'role': 'user', 'content': prompt},])
 
     return response['message']['content']
-
-
-if __name__ == '__main__':
-    user_prompt = 'Why is the sky blue?'
-    print(call_gpt(user_prompt))
