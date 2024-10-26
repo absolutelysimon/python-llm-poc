b2958db5db3959328fc118062fa905e24711b129
Catherine Fadamiro <catherine@catchafire.org>
2024-10-26 15:06:36 -0600

    Init logic to pass the right function

--- app.py
+++ app.py
@@ -20,10 +20,17 @@ def get_function_name(input_str):
             The string for this request is: '{input_str}'"""
         )
 
-        return response
+        # Todo: Hacky, find another way to do this
+        responselist = response.split("(")
+        function_name = responselist[0]
+        function_param = None
+        if len(function_name) > 1:
+            function_param = responselist[1].strip(")")
+        return (function_name, function_param)
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def execute_request():
@@ -32,10 +39,9 @@ def execute_request():
         return jsonify({'error': 'Missing input string'}), 400
 
     input_str = data['text']
-    function_name = get_function_name(input_str)
+    function_name, function_param = get_function_name(input_str)
 
-
-    # return jsonify({'status': 'success', 'result': f"Executed {function_name}"})
+    return jsonify({'status': 'success', 'result': f"Executed {function_name}"})
 
     # functions_status = getattr(functions, function_name)
 
